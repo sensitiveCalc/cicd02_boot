c4fd1db2d9e4b0bac456292538a9d05be8cdfd62
jeje <pearl903425@gmail.com>
2026-05-29 16:14:00 +0900

    마무리 커밋

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 first CI/CD TEST + 자동화 배포 !!";
+        return "GitHub Actions를 이용한 first CI/CD TEST + 곧 마무리 ..";
     }
 }
